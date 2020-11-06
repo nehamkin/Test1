28d08c3d8c78155542920619df329f632be82224
orrin <nehamkin@post.bgu.ac.il>
2020-11-06 13:56:27 +0200

    first test

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 int main(int argc, char** argv){
+    cout<<"Testing"<<endl;
     if(argc != 2){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
-    Session sess(argv[1]);
-    sess.simulate();
-    return 0;
+//    Session sess(argv[1]);
+//    sess.simulate();
+//    return 0;
 }
