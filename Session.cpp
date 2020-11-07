a383f183c9c7385dd4ad82a46c9a43f3b3415859
nehamkin <nehamkin@post.bgu.ac.il>
2020-11-07 16:44:44 +0200

    session1

--- Session.cpp
+++ Session.cpp
@@ -1,26 +1,19 @@
 //
-// Created by spl211 on 06/11/2020.
+// Created by spl211 on 07/11/2020.
 //
 
 #include "Session.h"
+
 Session::Session(const std::string &path) {
-     ifstream i(path);
-     json j;
-     j << i;
-     //----Tree Type----
-     string type=j["tree"];
-     if(type=="M")
-         treeType=MaxRank;
-     else if(type=="R")
-         treeType=Root;
-     else
-         treeType=Cycle;
-//    ----end Tree Type ----
-
+    ifstream i(path);
+    json j;
+    j << i;
+    //----Tree Type----
+    string type = j["tree"];
+    if (type == "M")
+        treeType = MaxRank;
+    else if (type == "R")
+        treeType = Root;
+    else
+        treeType = Cycle;
 }
-
-
-
-
-
-
