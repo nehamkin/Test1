81e3888f79e3201263585ce70a06a100759f8516
Talgnus <Talangus@gmail.com>
2020-11-07 15:44:44 +0200

    get arg from main parameters

--- main.cpp
+++ main.cpp
@@ -11,13 +11,13 @@ int main(int argc, char** argv){
         cout << "usage cTrace <config_path>" << endl;
         return 0;
     }
-   Session sess(argv[1]);
+  // Session sess(argv[1]);
 //      Session sess("config1.json");
 //    sess.simulate();
     ifstream i(argv[1]);
     json j2;
     j2<<i;
-    cout<<j2<<endl;
+    cout<<j2["tree"]<<endl;
 
     return 0;
 }
