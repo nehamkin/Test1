23da4c0eebd59a239f375620bec6fb6d88d8c975
nehamkin <nehamkin@post.bgu.ac.il>
2020-11-08 13:29:23 +0200

    tests

--- Tree.cpp
+++ Tree.cpp
@@ -6,13 +6,17 @@
 using namespace std;
 
 Tree::Tree(int rootLabel): node(rootLabel), children(){}
+Tree::Tree(const Tree& tree):children(tree.children),node(tree.node){}
 //Tree:: ~Tree() {if(children) delete children;}
 //Tree:: Tree(const Tree& other){
 //    node=other.node;
 //    children=other.children;
 //}
+Tree::Tree(const Tree &child){}
 CycleTree::CycleTree(int rootLabel, int currCycle) :Tree(rootLabel),currCycle(currCycle){}
 MaxRankTree::MaxRankTree(int rootLabel):Tree(rootLabel) {}
 RootTree::RootTree(int rootLabel) :Tree(rootLabel){}
 
-void Tree::addChild(const Tree &child) {this->children.push_back(&child);}
+void Tree::addChild(const Tree &child) {
+//
+}
