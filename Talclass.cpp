75a677c523cec76519adee47d1164f525e01402b
Talgnus <Talangus@gmail.com>
2020-11-09 14:16:43 +0200

    tracetree for root and cycle

--- Talclass.cpp
+++ Talclass.cpp
@@ -2,12 +2,5 @@
 // Created by spl211 on 09/11/2020.
 //
 
-#include "Talclass.h"
 
-int CycleTree::traceTree(){
-    int ans = this->getNode();
-    CycleTree* curr = this;
-    for(int counter = 0 ; counter<currCycle & curr!=0; counter++)
-        curr = dynamic_cast<CycleTree *>(this->getChildren().at(0));
-    return ans;
-}
+
