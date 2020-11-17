5efe35d3f85c36ac0f8097bab6f599400317054a
Talgnus <Talangus@gmail.com>
2020-11-17 19:15:34 +0200

    original begining

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,21 @@
-#include <iostream>
-#include <fstream>
+
 #include "../Include/Session.h"
-#include "../Include/json.hpp"
-#include "../Include/Tree.h"
+
 #include "../Include/Agent.h"
-#include <queue>
-#include "../Include/Graph.h"
+
 
 
 #define JSON_PATH "output.json"
 
 using namespace std;
 
-int tree_exp1(){
-    MaxRankTree t(1);
-    MaxRankTree t2(2);
-    t2.addChild(MaxRankTree(3));
-    t2.addChild(MaxRankTree(4));
-    t2.addChild(MaxRankTree(5));
-    t.addChild(t2);
-    t.addChild(MaxRankTree(6));
-    return t.traceTree();
-}
-
-
 int main(int argc, char** argv) {
+    if(argc != 2){
+        cout << "usage cTrace <config_path>" << endl;
+        return 0;
+    }
     nlohmann::json j;
-    Session sess("/home/spl211/Desktop/Test1/config4.json");
+    Session sess(argv[1]);
     sess.simulate();
     sess.output();
 }
